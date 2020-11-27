5009966ab7dcdf0cb8212e77d9f6d981eb9140cb
mlobacz <marcin.lobaczewski@gmail.com>
2020-11-27 20:01:42 +0100

    entry page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+import markdown2
 
 from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
 
+
+def entry(request, title):
+    """Renders requested wiki entry. Returns 404 if not found."""
+    entry = util.get_entry(title)
+    if not entry:
+        raise Http404(f"{title} entry was not found in the Wiki.")
+    return render(
+        request,
+        "encyclopedia/entry.html",
+        {"title": title, "entry": markdown2.markdown(entry)},
+    )
