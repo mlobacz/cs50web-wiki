d2f311d6a55ed9f60f4db5ca4f890a1add7de303
mlobacz <marcin.lobaczewski@gmail.com>
2020-11-29 17:47:18 +0100

    WIP search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import render, reverse, redirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django import forms
+
 import markdown2
 
 from . import util
 
+class SearchForm(forms.Form):
+    query = forms.CharField(label="Search Wiki")
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            return redirect("entry", title=form.cleaned_data["query"])
+        else:
+            form = SearchForm()
+    else:
+        form = SearchForm()
+    return render(request, "encyclopedia/index.html", {"entries": util.list_entries(), 'form': form})
 
 
 def entry(request, title):
@@ -19,3 +31,19 @@ def entry(request, title):
         "encyclopedia/entry.html",
         {"title": title, "entry": markdown2.markdown(entry)},
     )
+
+def search(request):
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect("test passed")
+        else:
+            form = SearchForm()
+    return render(request, "encyclopedia/index.html", {'form': form})
+    # entry = util.get_entry(query)
+    # if entry:
+    #     return render(
+    #         request,
+    #         "encyclopedia/entry.html",
+    #         {"title": query, "entry": markdown2.markdown(entry)},
+    #     )
