5bd2fe75c715b6b913ffe2abd0ba3254a4a127ee
mlobacz <marcin.lobaczewski@gmail.com>
2020-11-29 19:17:04 +0100

    working search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -22,8 +22,12 @@ def entry(request, title):
         {"title": title, "entry": markdown2.markdown(entry)},
     )
 
+
 def search(request):
-    if request.method == "GET":
-        entry = util.get_entry(request.GET["q"])
-        if entry:
-            return redirect('entry', title=entry)
+    """Redirects to page matching search query or returns list of entries containing query string."""
+    query = request.GET["q"]
+    if util.get_entry(query):
+        return redirect("entry", title=query)
+    else:
+        matching_entries = [entry for entry in util.list_entries() if query in entry]
+        return render(request, "encyclopedia/index.html", {"entries": matching_entries})
