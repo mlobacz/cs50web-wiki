ca8a5fa5ede938421d6640722089c6dd02f6e65d
mlobacz <marcin.lobaczewski@gmail.com>
2021-04-05 19:29:17 +0200

    add code quality tools usage

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("search", views.search, name="search"),
     path("new", views.new, name="new"),
     path("edit/<str:title>", views.edit, name="edit"),
-    path("random", views.random, name="random")
+    path("random", views.random, name="random"),
 ]
